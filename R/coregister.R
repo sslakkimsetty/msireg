2805837a5864e685149fd046d721f672f3b637d2
sslakkimsetty <saislakkimsetty@gmail.com>
2022-08-18 11:29:42 -0400

    housekeeping in coregister main method

--- R/coregister.R
+++ R/coregister.R
@@ -2,17 +2,18 @@
 #' @import Cardinal
 #' @import EBImage
 #' @import Rtsne
+#' @import BiocParallel
 
 
 coregister <- function(mse, opt, mse_roi=NULL, opt_roi=NULL,
                        SSC=TRUE, mz_list=NULL, verbose=FALSE) {
     mse <- Cardinal::process(mse) # <- process pending operations, if any
-    mse_params <- list(
+    mse_attrs <- list(
         nX = dims(mse)[1], nY = dims(mse)[2],
         nF = dim(mse)[1], nP = dim(mse)[2],
         nXo = dim(opt)[1], nYo = dim(opt)[2]
     )
-    isFull <- (mse_params$nX * mse_params$nY) == (mse_params$nP)
+    isFull <- (mse_attrs$nX * mse_attrs$nY) == (mse_attrs$nP)
 
     ##### Sanity checks #####
     # 1. [Are all pixels present] AND [is mse_roi NA]? <- yes = bad
@@ -29,14 +30,14 @@ coregister <- function(mse, opt, mse_roi=NULL, opt_roi=NULL,
     if (!isFull & is.null(mse_roi)) {
         message("Co-registration performs better on images with their backgrounds removed ...")
         message("Inferring tissue to be the pixels present ... \n")
-        mse_roi <- constructROIFromMSIImage(mse, pars=mse_params)
+        mse_roi <- constructROIFromMSIImage(mse, pars=mse_attrs)
     }
 
     # 3. [Are some pixels missing] AND [is mse_roi not NA]?
     if (!isFull & !is.null(mse_roi)) {
-        if (length(mse_roi) == mse_params$nP) { # need to rasterize
+        if (length(mse_roi) == mse_attrs$nP) { # need to rasterize
             mse_roi <- rasterizeROIFromCardinal(mse, mse_roi)
-        } else if (length(mse_roi) != mse_params$nX * mse_params$nY) {
+        } else if (length(mse_roi) != mse_attrs$nX * mse_attrs$nY) {
             message("mse_roi parameter is not passed properly (incorrect size)")
             message("Select the tissue outline using Cardinal::SelectROI()")
             mse_roi <- Cardinal::selectROI(mse, mz=mz(mse)[1])
@@ -68,13 +69,13 @@ coregister <- function(mse, opt, mse_roi=NULL, opt_roi=NULL,
 
     # Construct 3-channeled MSI image
     if (length(mz(mse_sub)) == 3) {
-        msimg <- array(ints, dim=c(mse_params$nY, mse_params$nX, 3))
+        msimg <- array(ints, dim=c(mse_attrs$nY, mse_attrs$nX, 3))
         msimg <- Image(aperm(msimg, perm=c(2,1,3)), colormode=Color)
     } else {
         # t-SNE representation
         message("performing tsne ... \n")
         msimg <- .Rtsne(ints[t(mse_roi), ], tissue=mse_roi,
-                        attrs=mse_params)
+                        attrs=mse_attrs)
     }
     print(dim(msimg))
     msimg <- normalizeImage(msimg, contrast.enhance="histogram")
@@ -103,23 +104,12 @@ coregister <- function(mse, opt, mse_roi=NULL, opt_roi=NULL,
 
 ..coregister <- function(mse, opt, mse_roi=NULL, opt_roi=NA,
                          SSC=TRUE, mz_list=NULL, verbose=FALSE, .data=NULL) {
-    opt <- .data$opt
-    msimg <- .data$msimg
     fixed <- .data$fixed
     moving <- .data$moving
-
-    # # Convert to grayscale images
-    # opt <- channel(opt, "luminance")
-    # msimg <- channel(msimg, "luminance")
-
-    # SimpleITK init
-    # fixed <- SimpleITK::as.image(imageData(opt), isVector=FALSE)
-    # moving <- SimpleITK::as.image(imageData(msimg), isVector=FALSE)
 
     # Registration
     outTx <- coregisterWithSimpleITK(fixed, moving)
     list(
-        opt=opt, msimg=msimg,
         fixed=fixed, moving=moving,
         outTx=outTx
     )
