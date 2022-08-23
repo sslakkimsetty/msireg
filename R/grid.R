db0c2c63a49a797f06da378cca47959a533bb6e6
sslakkimsetty <saislakkimsetty@gmail.com>
2022-08-23 16:51:00 -0400

    added color functionality to grid overlays

--- R/grid.R
+++ R/grid.R
@@ -1,6 +1,6 @@
 
 
-overlayGridOnImage <- function(img, color="#ffffff",
+overlayGridOnImage <- function(img, color=NULL,
                                scale_factor=4, overlay_weight=0.8) {
     out <- list()
     grid <- constructNDGrid(img, scale_factor=scale_factor)
@@ -12,10 +12,15 @@ overlayGridOnImage <- function(img, color="#ffffff",
     grid <- resizeAndPadImageToMatchDims(grid, dim(img)[1:2])
     grid <- normalizeImage(grid)
 
-    out$grid <- .addColorToImage(img=grid, color=color)
+    if (is.null(color)) {
+        out$grid <- grid
+    } else {
+        out$grid <- .addColorToImage(img=grid, color=color)
+        out$color_overlay <- .addColorToImage(img) * overlay_weight +
+            out$grid * (1 - overlay_weight)
+    }
+
     out$overlay <- img * overlay_weight + grid * (1 - overlay_weight)
-    out$color_overlay <- .addColorToImage(img) * overlay_weight +
-        .addColorToImage(grid) * (1 - overlay_weight)
     out
 }
 
@@ -56,16 +61,21 @@ constructNDGrid <- function(img, scale_factor=4) {
 }
 
 
-.addColorToImage <- function(img, color="#ffffff") {
+.addColorToImage <- function(img, color="#ffffff", alpha=NULL) {
     dim <- dim(img)
-    channels <- 3
+    ch <- if (is.null(alpha)) 3 else 4
 
     color_vec <- as.vector(col2rgb(color) / 255)
+    if (!is.null(alpha)) color_vec <- c(color_vec, 1) # 1 = transparent
     color_arr <- array(rep(color_vec, prod(dim(img))),
-                       dim=c(dim(img), 3)[c(3,1,2)])
+                       dim=c(dim(img), ch)[c(3,1,2)])
     color_arr <- aperm(color_arr, c(2,3,1))
 
-    img_arr <- array(rep(imageData(img), 3), dim=c(dim(img), 3))
+    img_arr <- array(rep(imageData(img), ch), dim=c(dim(img), ch))
+    if (!is.null(alpha)) {
+        img_arr[, , 4] <- 0
+        color_arr[, , 4][color_arr[, , 1]==1] <- 0 # 0 = opaque
+    }
     Image(img_arr * color_arr, colormode=Color)
 }
 
