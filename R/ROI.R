9f3b5e81c98189c3a8cdbd1ef1640fdae5d518e1
sslakkimsetty <saislakkimsetty@gmail.com>
2022-08-29 10:52:19 -0400

    added importFrom tag for rgb

--- R/ROI.R
+++ R/ROI.R
@@ -1,9 +1,9 @@
 
 
-#' Select multiple regions-of-interest (ROIs) on Mass Spectrometry image 
-#' 
-#' `multiSelectROI()` extends the `Cardinal::selectROI()` method to 
-#'     allow selction of multiple disconnected tissues in the image. 
+#' Select multiple regions-of-interest (ROIs) on Mass Spectrometry image
+#'
+#' `multiSelectROI()` extends the `Cardinal::selectROI()` method to
+#'     allow selction of multiple disconnected tissues in the image.
 #'
 #' @param mse The imaging dataset to select ROI
 #' @param mz An example ion image with mass / charge (mz) to display; passed
@@ -27,7 +27,7 @@ multiSelectROI <- function(mse, mz, rasterize=TRUE, ...) {
     while (sel) {
         .roi <- selectROI(mse, mz=mz, ...)
         roi <- (roi | .roi)
-        sel <- askYesNo(paste0("Do you want to select another ", 
+        sel <- askYesNo(paste0("Do you want to select another ",
             "ROI on the same tissue?"))
     }
 
@@ -36,10 +36,10 @@ multiSelectROI <- function(mse, mz, rasterize=TRUE, ...) {
 }
 
 
-#' Select multiple regions-of-interest (ROIs) on optical image 
-#' 
-#' `multiDrawROI()` extends `drawROIOnImage()` to allow selection of 
-#'     multiple disconnected tissues in the optical image. 
+#' Select multiple regions-of-interest (ROIs) on optical image
+#'
+#' `multiDrawROI()` extends `drawROIOnImage()` to allow selection of
+#'     multiple disconnected tissues in the optical image.
 #'
 #' @param img An `EBImage::Image` object to select ROI on
 #'
@@ -61,11 +61,11 @@ multiDrawROI <- function(img) {
     while (sel) {
         .roi <- drawROIOnImage(img)
         roi <- (roi | .roi)
-        sel <- askYesNo(paste0("Do you want to select another ", 
+        sel <- askYesNo(paste0("Do you want to select another ",
             "ROI on the same tissue?"))
     }
     roi
-} 
+}
 
 
 #' Select a single region-of-interest (ROI) on optical image
@@ -92,12 +92,14 @@ drawROIOnImage <- function(img) {
     }
     out <- point.in.polygon(coord[, 1], coord[, 2], loc$x, loc$y)
     out <- matrix(out, nrow=nX)
-} 
+}
 
 
-# Helper function to draw marks, lines and finally the polygon 
-#     as the user selects points using the ROI methods 
+# Helper function to draw marks, lines and finally the polygon
+#     as the user selects points using the ROI methods
 #     - function taken from the Cardinal package
+
+#' @importFrom grDevices col2rgb rgb
 .locator <- function() {
     xs <- numeric()
     ys <- numeric()
@@ -113,16 +115,16 @@ drawROIOnImage <- function(img) {
     }
     polygon(xs, ys, col=rgb(1,1,1,0.5))
     list(x=xs, y=ys)
-} 
+}
 
 
-#' Apply binary mask (ROI) on the image to retain only the region of 
+#' Apply binary mask (ROI) on the image to retain only the region of
 #'     interest
 #'
 #' @param img An `EBImage::Image` object to apply ROI on
 #'
-#' @return Returns the image with only the region of interest and the 
-#'     background removed 
+#' @return Returns the image with only the region of interest and the
+#'     background removed
 #'
 #' @export
 #'
@@ -138,23 +140,23 @@ applyROIOnImage <- function(img, roi) {
 }
 
 
-# `Cardinal::selectROI()` returns a logical vector of length equal 
-#     to the existing pixels. `rasterizeROIFromCardinal()` builds 
-#     a rasterized matrix of ROI (binary mask) using coord data 
+# `Cardinal::selectROI()` returns a logical vector of length equal
+#     to the existing pixels. `rasterizeROIFromCardinal()` builds
+#     a rasterized matrix of ROI (binary mask) using coord data
 #     from `mse`
 
 rasterizeROIFromCardinal <- function(mse, roi, byrow=FALSE) {
     out <- matrix(rep(FALSE, prod(dims(mse))), nrow=dims(mse)[1])
-    out[as.matrix(coord(mse))] <- roi # coord(mse) is row major and 
-    # Cardinal's selectROI logical vector is also row major 
+    out[as.matrix(coord(mse))] <- roi # coord(mse) is row major and
+    # Cardinal's selectROI logical vector is also row major
 
     if (byrow) t(out)
     else out
 }
 
 
-# If ROI is not provided in arguments of `msireg::coregister()`, ROI 
-#     is inferred to be the existing pixels in the MSI data. 
+# If ROI is not provided in arguments of `msireg::coregister()`, ROI
+#     is inferred to be the existing pixels in the MSI data.
 
 constructROIFromMSIImage <- function(mse, attrs=NULL) {
     roi <- matrix(rep(FALSE, attrs$nX * attrs$nY), ncol=attrs$nY)
