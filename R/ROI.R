7a9abe52656f000aa63e80d0e44783a5a28f113d
sslakkimsetty <saislakkimsetty@gmail.com>
2022-09-06 17:07:25 -0400

    added: methods and helpers to spatially match optical and msi tissues

--- R/ROI.R
+++ R/ROI.R
@@ -6,10 +6,10 @@
 #'     allow selction of multiple disconnected tissues in the image.
 #'
 #' @param mse The imaging dataset to select ROI
-#' @param mz An example ion image with mass / charge (mz) to display; passed
-#'     over to `Cardinal::image()` function
-#' @param rasterize A logical flag (default = TRUE); whether to rasterize the
-#'     ROI (affects the cases only where MS image is not rectangular).
+#' @param mz An example ion image with mass / charge (mz) to display; 
+#'     passed over to `Cardinal::image()` function
+#' @param rasterize A logical flag (default = TRUE); whether to rasterize 
+#'     the ROI (affects the cases only where MS image is not rectangular).
 #' @param ... Passed to `Cardinal::selectROI()`
 #'
 #' @return Returns the ROI as a logical matrix if `rasterize` is TRUE or a
@@ -43,8 +43,8 @@ multiSelectROI <- function(mse, mz, rasterize=TRUE, ...) {
 #'
 #' @param img An `EBImage::Image` object to select ROI on
 #'
-#' @return Returns the ROI as a logical matrix of the same spatial dimensions
-#'     as `img`
+#' @return Returns the ROI as a logical matrix of the same spatial 
+#'     dimensions as `img`
 #'
 #' @importFrom graphics locator points polygon
 #' @importFrom sp point.in.polygon
@@ -72,8 +72,8 @@ multiDrawROI <- function(img) {
 #'
 #' @param img An `EBImage::Image` object to select ROI on
 #'
-#' @return Returns the ROI as a logical matrix of the same spatial dimensions
-#'     as `img`
+#' @return Returns the ROI as a logical matrix of the same spatial 
+#'     dimensions as `img`
 #'
 #' @importFrom graphics locator points polygon
 #' @importFrom sp point.in.polygon
@@ -165,6 +165,105 @@ constructROIFromMSIImage <- function(mse, attrs=NULL) {
     roi[coords] <- TRUE
 
     roi
+} 
+
+
+
+# cropToEdgesAndPadZeros() builds target image and its corresponding 
+#     (spatially matching) target mask to approximately match the 
+#     spatial position the tissue (ROI) on the optical image with that of 
+#     the MSI image. 
+
+cropToEdgesAndPadZeros <- function(ref_mask, target_mask, 
+    target_img=NULL) {
+    # Cast target_img as Image as it's needed down the line
+    if (is.matrix(target_img)) {
+        target_img <- Image(target_img, colormode=Grayscale)
+    } else if (is.array(target_img)) {
+        target_img <- Image(target_img, colormode=Color)
+    }
+
+    dims <- length(dim(target_img))
+    pads <- getPadPercentagesFromMask(ref_mask)
+    edges <- findBoundaryEdgesInMask(target_mask)
+
+    # Cropping
+    target_mask <- target_mask[edges[3]:edges[4], edges[1]:edges[2]]
+
+    if (dims == 2) {
+        target_img <- target_img[edges[3]:edges[4], edges[1]:edges[2]]
+    } else if (dims > 2) {
+        target_img <- target_img[edges[3]:edges[4], edges[1]:edges[2], ]
+    }
+
+    # Padding
+    target_mask <- padImageWithPercentage(
+        Image(target_mask), pad=c(pads[3], pads[2], pads[1], pads[4]))
+    target_mask <- imageData(target_mask)
+    target_mask <- matrix(as.logical(target_mask), nrow=nrow(target_mask))
+
+    target_img <- padImageWithPercentage(
+        target_img, pad=c(pads[3], pads[2], pads[1], pads[4]))
+
+    list(mask=target_mask, img=target_img)
+} 
+
+
+# getPadPercentagesFromMask() evaluates how much padding (or FALSEs) are 
+#     present on alll four edges of the tissue (TRUEs); the order of 
+#     indices returned are from the c(left, right, top, bottom). 
+#     Subsetting using these edges should be in reverse, 
+#     i.e., mask[rows, cols]
+
+getPadPercentagesFromMask <- function(mask) {
+    edges <- findBoundaryEdgesInMask(mask)
+    edges[1:2] <- edges[1:2] / dim(mask)[2]
+    edges[3:4] <- edges[3:4] / dim(mask)[1]
+    edges[c(2, 4)] <- 1 - edges[c(2, 4)] # 2, 4 finds boundary on far side
+
+    edges
 }
 
+
+# findBoundaryEdgesInMask() returns a vector of indices of ROI / mask 
+#     boundaries; the order of indices returned are from the c(left, 
+#     right, top, bottom). 
+#     Subsetting using these edges should be in reverse, 
+#     i.e., mask[rows, cols]
+
+findBoundaryEdgesInMask <- function(mask) {
+    edges <- rep(0, 4)
+    edges[1] <- .findBoundaryEdgeOfCols(mask, cols=c(1:dim(mask)[2]))
+    edges[2] <- .findBoundaryEdgeOfCols(mask, cols=c(dim(mask)[2]:1))
+    edges[3] <- .findBoundaryEdgeOfRows(mask, rows=c(1:dim(mask)[1]))
+    edges[4] <- .findBoundaryEdgeOfRows(mask, rows=c(dim(mask)[1]:1))
+
+    edges
+}
+
+
+# .findBoundaryEdgeOfRows() finds the first row at which the boundary
+#     encounters a TRUE. `row` is a list of row numbers, and the method 
+#     follows it sequentially. 
+
+.findBoundaryEdgeOfRows <- function(mask, rows=NULL) {
+    for (row in rows) {
+        if (any(mask[row, ])) {
+            return(row)
+        }
+    }
+}
+
+
+# .findBoundaryEdgeOfCols() finds the first column at which the boundary
+#     encounters a TRUE. `cols` is a list of column numbers, and the 
+#     method follows it sequentially. 
+
+.findBoundaryEdgeOfCols <- function(mask, cols=NULL) {
+    for (col in cols) {
+        if (any(mask[, col])) {
+            return(col)
+        }
+    }
+}
 
