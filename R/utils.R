2be1c2540b31f5257147e39b3de461b532e5315d
sslakkimsetty <saislakkimsetty@gmail.com>
2022-09-09 16:40:01 -0400

    housekeeping and unintentional verbose fixed

--- R/utils.R
+++ R/utils.R
@@ -1,12 +1,15 @@
 #' @importFrom stats quantile rnorm
 
 normalizeImage <- function(img, separate=TRUE, ft=c(0,1),
-                           contrast.enhance=c("none", "suppression", "histogram"),
+                           contrast.enhance=c("none", "suppression",
+                                              "histogram"),
                            smooth.image=c("none", "gaussian", "adaptive"),
-                           suppression=c(0.01, 0.99)) {
+                           suppression=c(0.01, 0.99), verbose=FALSE) {
 
-    contrast.enhance <- match.arg(contrast.enhance, c("none", "suppression", "histogram"))
-    if (contrast.enhance == "suppression") contrast.enhance <- contrast.enhance.suppression
+    contrast.enhance <- match.arg(contrast.enhance,
+                                  c("none", "suppression", "histogram"))
+    if (contrast.enhance == "suppression") contrast.enhance <-
+            contrast.enhance.suppression
     contrast.enhance <- Cardinal:::contrast.enhance.method(contrast.enhance)
 
     smooth.image <- match.arg(smooth.image, c("none", "gaussian", "adaptive"))
@@ -23,7 +26,8 @@ normalizeImage <- function(img, separate=TRUE, ft=c(0,1),
 
     if (isTRUE(separate)) {
         . <- sapply(c(1:dim(img)[3]), function(i) {
-            ints[, , i] <<- normalize.image(smooth.image(contrast.enhance(ints[, , i])))
+            ints[, , i] <<- normalize.image(
+                smooth.image(contrast.enhance(ints[, , i])))
         })
     } else {
         . <- sapply(c(1:dim(img)[3]), function(i) {
@@ -34,13 +38,16 @@ normalizeImage <- function(img, separate=TRUE, ft=c(0,1),
 }
 
 
-contrast.enhance.suppression <- function(x, suppression=c(0, 1)) {
+contrast.enhance.suppression <- function(x, suppression=c(0, 1),
+                                         verbose=FALSE) {
     if (all(is.na(x))) return(x)
 
     cutoff_min <- quantile(x, suppression[0], na.rm=TRUE)
     cutoff_max <- quantile(x, suppression[1], na.rm=TRUE)
-    message(paste0(c("cutoff_min is ", cutoff_min)))
-    message(paste0(c("cutoff_max is ", cutoff_max)))
+    if (verbose) {
+        message(paste0(c("cutoff_min is ", cutoff_min)))
+        message(paste0(c("cutoff_max is ", cutoff_max)))
+    }
 
     if (isTRUE(cutoff_min > min(x, na.rm=TRUE))) x[x < cutoff_min] <- cutoff_min
     if (cutoff_max > min(x, na.rm=TRUE)) x[x > cutoff_max] <- cutoff_max
